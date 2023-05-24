e4e348aff61f312c527829dbf27c18a9a802f298
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-24 10:32:00 -0700

    expo update

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_KEY } from '@env';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
