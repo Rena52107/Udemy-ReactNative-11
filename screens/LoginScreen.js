ea955acb7e1508c213c8a946c347b9938f2408fb
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-24 11:02:38 -0700

    add login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message='Loggin you in...' />;
+  }
+  return (
+    <AuthContent
+      isLogin
+      onAuthenticate={signupHandler}
+    />
+  );
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 import { API_KEY } from '@env';
 
+async function authenticate(mode, email, password) {
+  console.log(API_KEY);
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate('signUp', email, password);
+}
+
+async function login(emial, password) {
+  await authenticate('signInWithPassword', emial, password);
 }
