986a7e74daf180ee0ed7e7c618f8e8e83b1f08f1
Rena Sakata <rena.sakata.cn@gmail.com>
2023-05-25 17:18:20 -0700

    Remove console.log

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { API_KEY } from '@env';
 
 async function authenticate(mode, email, password) {
-  console.log(API_KEY);
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
   const response = await axios.post(url, {
